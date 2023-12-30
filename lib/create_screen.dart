4eefc255f0216a175e7d3db770c955ee3751d3f7
Sangin Han <hugedreamhd@gmail.com>
2023-12-30 23:05:23 +0900

    DB에 TODO 내용을 등록한다. navigator.pop을 할때 aync 값이 늦어서 화면 exception이 일어나는 것을 방지한다(if(mounted))

--- lib/create_screen.dart
+++ lib/create_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:todolist/main.dart';
+import 'package:todolist/todo.dart';
 
-class CreateScreen extends StatelessWidget {
+class CreateScreen extends StatefulWidget {
   const CreateScreen({super.key});
+
+  @override
+  State<CreateScreen> createState() => _CreateScreenState();
+}
+
+class _CreateScreenState extends State<CreateScreen> {
+  final _textController = TextEditingController(); //할일을 입력하세요 칸에 들어온 값을 담는다
+
+  @override
+  void dispose() {
+    _textController.dispose(); //메모리 해제 필수
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,7 +25,20 @@ class CreateScreen extends StatelessWidget {
         title: Text('Todo 작성'),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: () async {
+              await todos.add(
+                  //db를 조작하는 모든 기능은 futuer 오래걸릴 수 있다. 끝나기 전에 pop할수가 있다.
+                  Todo(
+                title: _textController.text,
+                dateTime: DateTime.now().microsecondsSinceEpoch,
+              ));
+
+              if (mounted) {
+                //mounted = 화면이 잘 표시가 되는가
+                Navigator.pop(
+                    context); //노랸 라인 - async 키워드가 있을 때 context 가 동작가능 한지 확인해봐라
+              }
+            },
             icon: const Icon(Icons.done),
           ),
         ],
@@ -18,6 +46,7 @@ class CreateScreen extends StatelessWidget {
       body: Padding(
         padding: const EdgeInsets.all(8.0),
         child: TextField(
+          controller: _textController, //텍스트 값을 얻는다
           decoration: InputDecoration(
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(10.0),
--- lib/main.dart
+++ lib/main.dart
@@ -6,11 +6,11 @@ import 'package:todolist/todo.dart';
 late final Box<Todo> todos; //탑레벨(아무데서나 접근가능한 곳)에 늦게 값을 초기화 하겠다 밑에 todos에서
 
 Future<void> main() async {
-  await Hive.initFlutter; //Hive 초기화 await를 왜 제거해도 된다고 그러지?
+  await Hive.initFlutter(); //Hive 초기화 await를 왜 제거해도 된다고 그러지?
 //Hive가 todo라는 객체를 알 수 있도록 알려줘야 한당
-  Hive.registerAdapter(TodoAdapter());//준비 완료
+  Hive.registerAdapter(TodoAdapter()); //준비 완료
 
-  Box<Todo> todos = await Hive.openBox<Todo>('todolist.db');//db이름 작성 후에 그에 맞는 타입을 <generic>안에 넣는다
+  todos = await Hive.openBox<Todo>('todolist.db'); //db이름 작성 후에 그에 맞는 타입을 <generic>안에 넣는다 - 초기화
   //todos 객체를 통해서 db에 접근 가능
   runApp(const MyApp());
 }
