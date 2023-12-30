a34d4cd4aab97bd14a8fd2d3bf806642094441f0
Sangin Han <hugedreamhd@gmail.com>
2023-12-30 22:42:16 +0900

    Mock데이터는 삭제하고 실제  todos안에 있는 값으로 ListView를 연결시켜준다 todos.values로 실제값을 연결하고 가져온다

--- lib/list_screen.dart
+++ lib/list_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:todolist/create_screen.dart';
+import 'package:todolist/main.dart';
 import 'package:todolist/todo.dart';
 
 class ListScreen extends StatefulWidget {
@@ -12,20 +13,20 @@ class ListScreen extends StatefulWidget {
 class _ListScreenState extends State<ListScreen> {
   //변수나 상수를 작성하면, stateless위젯은 사용할 수 없게 된다
   //스테이트풀 위젯으로 바꾼당
-  final todos = [
-    Todo(
-      title: 'title 1',
-      dateTime: 123123,
-    ),
-    Todo(
-      title: 'title 2',
-      dateTime: 12312223,
-    ),
-    Todo(
-      title: 'title 3',
-      dateTime: 12312223,
-    ),
-  ];
+  // final todos = [
+  //   Todo(
+  //     title: 'title 1',
+  //     dateTime: 123123,
+  //   ),
+  //   Todo(
+  //     title: 'title 2',
+  //     dateTime: 12312223,
+  //   ),
+  //   Todo(
+  //     title: 'title 3',
+  //     dateTime: 12312223,
+  //   ),
+  // ];
 
   @override
   Widget build(BuildContext context) {
@@ -33,12 +34,14 @@ class _ListScreenState extends State<ListScreen> {
       appBar: AppBar(
         title: const Text('Todo 리스트'),
       ),
-      body: ListView(//List있는 값을 변환해서 표현할때는 map 함수를 쓰면 된다
-        children: todos
+      body: ListView(
+        //List있는 값을 변환해서 표현할때는 map 함수를 쓰면 된다
+        children: todos.values
             .map((todo) => ListTile(
-          title: Text(todo.title),
-          subtitle: Text('${todo.dateTime}'),//int라 String으로 바꾸려고
-        )).toList(),//Iterable로 반환되서 List형태로 바꿔줘야한다
+                  title: Text(todo.title),
+                  subtitle: Text('${todo.dateTime}'), //int라 String으로 바꾸려고
+                ))
+            .toList(), //Iterable로 반환되서 List형태로 바꿔줘야한다
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
