a614a95c9496c00e55504df8fc7d249c7811183b
Sangin Han <hugedreamhd@gmail.com>
2023-12-30 22:36:17 +0900

    HiveObject 를 이용해 데이트베이스를 만들고 todo.g.dart 파일이 자동으로 생성되고 main파일에서 db이름 작성 및 todos를 통해 db에 접근 가능하게 만들었다

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:todolist/list_screen.dart';
+import 'package:todolist/todo.dart';
 
-void main() {
+late final Box<Todo> todos; //탑레벨(아무데서나 접근가능한 곳)에 늦게 값을 초기화 하겠다 밑에 todos에서
+
+Future<void> main() async {
+  await Hive.initFlutter; //Hive 초기화 await를 왜 제거해도 된다고 그러지?
+//Hive가 todo라는 객체를 알 수 있도록 알려줘야 한당
+  Hive.registerAdapter(TodoAdapter());//준비 완료
+
+  Box<Todo> todos = await Hive.openBox<Todo>('todolist.db');//db이름 작성 후에 그에 맞는 타입을 <generic>안에 넣는다
+  //todos 객체를 통해서 db에 접근 가능
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
--- lib/todo.dart
+++ lib/todo.dart
@@ -1,7 +1,18 @@
-class Todo {
+import 'package:hive/hive.dart';
+part 'todo.g.dart'; //build_runner 할 때 오류생기면 넣는다
+@HiveType(typeId: 0)//Hive라는 데이터베이스에 저장 할 수 있는 객체가 된다
+class Todo extends HiveObject {
+  //id를 통해서 unique한 객체를 판단하는 근거가 된다
   // 클래스 만들 때는 내가 표현하고자 하는 객체를 보고 그 속성을 도출해내면 된다
   //이 안에는 필드라 부른다
+  @HiveField(0)//flutter pub run build_runner build
+
+  int? id;
+
+  @HiveField(1)
   String title;
+
+  @HiveField(2)
   int dateTime; // int 값으로 저장하고 변환해서 표시해보자
 
   Todo({
@@ -10,6 +21,6 @@ class Todo {
   }); //필수 생성자(named parameter)
 }
 
-//TOdo('내용', 123123)
+//Todo('내용', 123123)
 //좀 더 플러터 스럽게는
 //Todo(title: '내용', dateTime 1233);
