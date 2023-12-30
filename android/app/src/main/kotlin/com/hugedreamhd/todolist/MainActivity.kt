647b8a2e5acef81606765ffc4c4b0874fcf9412f
Sangin Han <hugedreamhd@gmail.com>
2023-12-30 21:13:18 +0900

    todoList 첫 화면 Appbar, body, flotingactionbutton  작성 body는 ListView와 ListTile로 만듬

---
+++ android/app/src/main/kotlin/com/hugedreamhd/todolist/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.hugedreamhd.todolist
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/list_screen.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+class ListScreen extends StatelessWidget {
+  const ListScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Todo 리스트'),
+      ),
+      body: ListView(
+        children: const [
+          ListTile(
+            title: Text('title 1'),
+            subtitle: Text('subtitle 1'),
+          ),
+          ListTile(
+            title: Text('title 1'),
+            subtitle: Text('subtitle 1'),
+          ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {  },
+        child: Icon(Icons.add),
+
+      ),
+    );
+  }
+}
